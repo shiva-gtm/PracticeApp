4499b0d39e206dfd24ee96eb3399c79d64cc707e
Shiva Gautam <shivagautam2054@.com>
2025-05-31 14:31:15 +0545

    Practice Listview

--- practiceapp/lib/main.dart
+++ practiceapp/lib/main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Demo App'),
+        ),
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  title: Text("Item $index"),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+}

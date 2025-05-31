b1fa37d454420994a0602a07d4aa600315ab1fbd
Shiva Gautam <shivagautam2054@.com>
2025-05-31 16:45:00 +0545

    API intergration done

--- practiceapp/lib/main.dart
+++ practiceapp/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// String? stringResponse;
-// Map? mapResponse;
-// Map? dataResponse;
+Map? mapResponse;
+Map? dataResponse;
+List? listResponse;
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -30,44 +30,80 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  // Future apicall() async {
-  //   http.Response response;
-  //   response = await http.get(Uri.parse('https://reqres.in/api/users/2'));
-  //   if (response.statusCode == 200) {
-  //     setState(() {
-  //       // stringResponse = response.body;
-  //       mapResponse = json.decode(response.body);
-  //       dataResponse = mapResponse!['data'];
-  //     });
-  //   }
-  // }
+  Future apicall() async {
+    http.Response response;
+    response = await http.get(Uri.parse('https://reqres.in/api/users?page=2'));
+    if (response.statusCode == 200) {
+      setState(() {
+        mapResponse = json.decode(response.body);
+        listResponse = mapResponse!['data'];
+      });
+    }
+  }
 
-  // @override
-  // void initState() {
-  //   apicall();
-  //   super.initState();
-  // }
+  @override
+  void initState() {
+    apicall();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 165, 166, 165),
       appBar: AppBar(
         title: const Text('API Demo'),
         centerTitle: true,
       ),
-      // body: Center(
-      //   child: Container(
-      //     height: 200,
-      //     width: 300,
-      //     decoration: BoxDecoration(
-      //         borderRadius: BorderRadius.circular(20), color: Colors.blue),
-      //     child: Center(
-      //       child: dataResponse == null
-      //           ? const Text('Data is loading')
-      //           : Text(dataResponse!['email'].toString()),
-      //     ),
-      //   ),
-      // ),
+      body: ListView.builder(
+        itemCount: listResponse == null ? 0 : listResponse?.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: Card(
+              shadowColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: listResponse == null
+                          ? const Text("Data is loading")
+                          : Text(
+                              '${listResponse![index]!['first_name']} ${listResponse![index]!['last_name']}',
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                    ),
+                    const SizedBox(height: 10),
+                    Center(
+                      child: Image.network(
+                        listResponse![index]!['avatar'],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    listResponse == null
+                        ? const Text("Data is loading")
+                        : Center(
+                            child: Text(
+                              'email: ${listResponse![index]!['email']}',
+                              style: const TextStyle(fontSize: 15),
+                            ),
+                          ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
